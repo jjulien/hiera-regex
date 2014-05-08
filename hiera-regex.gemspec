19fa36629a6938bf72d697fb5de19cb6aa8d5591
John Julien <john@julienfamily.com>
2014-05-07 19:54:26 -0500

    Initial Commit

---
+++ hiera-regex.gemspec
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |gem|
+  gem.name         = "hiera-regex"
+  gem.version      = "0.1.0"
+  gem.description  = "Hiera backend for looking up keys using the power of regular expressions"
+  gem.summary      = "Hiera regex backend"
+  gem.author       = "John Julien"
+  gem.license      = "Apache 2.0"
+  gem.homepage     = "http://github.com/jjulien/hiera-regex"
+  gem.files        = Dir['lib/**/*'] + ['LICENSE']
+end
---
+++ lib/hiera/backend/regex_backend.rb
@@ -0,0 +1,68 @@
+class Hiera
+  module Backend
+    class Regex_backend
+
+      # TODO: Support JSON or YAML format and initialize accordingly
+      def initialize
+         require 'yaml'
+      end
+
+      # TODO: Support multiple resolution_types, currently only supports :priority
+      def lookup(key, scope, order_override, resolution_type)
+         answer = nil
+         datasourcefiles(scope) do |source|
+           scope_key = File.basename(source, ".regex")
+           scope_key = "::#{scope_key}" if ! scope.has_key?(scope_key)
+           if ! scope.has_key?(scope_key)
+             Hiera.debug("Could not find key #{scope_key} within scope, skipping check for source #{source}")
+             next
+           end
+           Hiera.debug("Checking #{source} for #{scope_key} regex matching #{scope[scope_key]}")
+           # TODO: Add support for more than just YAML format
+           data = YAML.load_file(source)
+           next if ! data
+           next if data.empty?
+
+           lineno = 0
+           data.each do |item|
+             lineno = lineno + 1
+             item.each_key do |regex_key|
+               if scope[scope_key] =~ /#{regex_key}/ and item[regex_key][key]
+                 Hiera.debug("#{scope_key} with value of '#{scope[scope_key]}' matched regex /#{regex_key}/ at #{source}:#{lineno}")
+                 answer = item[regex_key][key]
+                 break
+               end
+             end
+             break if answer
+           end
+         end
+         return answer
+       end
+
+       def datasources(scope)
+         datadir = Backend.datadir(:regex, scope)
+         Config[:hierarchy].flatten.map do |source|
+           # We only support data sources that end in a key.  Keys found
+           # in the middle of a datasource's name are impossible to support since
+           # standard file names do not do well containing regular experssion patterns
+           if source =~ /\}$/
+             # We need to strip out the ending key's %{::} characters so that they do not
+             # get interpolated when we call parse_string
+             yield(Backend.parse_string(source.gsub(/%\{:*([^\{]*)\}$/, '\1'), scope))
+           end
+         end
+       end
+
+       def datasourcefiles(scope)
+         datadir = Backend.datadir(:regex, scope)
+         datasources(scope) do |source|
+           file = Backend.datafile(:regex, scope, source, :regex)
+           if file
+             yield(file)
+           end
+         end
+       end
+
+    end
+  end
+end   
