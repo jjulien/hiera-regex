27530fc2e064ec730934567e32a155059a094be7
Calvin Leung Huang <cleung2010@gmail.com>
2015-01-05 10:30:58 -0500

    Fixed spacing, added hiera hash and array merging

--- lib/hiera/backend/regex_backend.rb
+++ lib/hiera/backend/regex_backend.rb
@@ -12,70 +12,83 @@ class Hiera
 
       # TODO: Support multiple resolution_types, currently only supports :priority
       def lookup(key, scope, order_override, resolution_type)
-         answer = nil
-         datasourcefiles(scope) do |source|
-           scope_key = File.basename(source, ".regex")
-           scope_key = "::#{scope_key}" if ! scope[scope_key]
-           if ! scope[scope_key]
-             Hiera.debug("Could not find key #{scope_key} within scope, skipping check for source #{source}")
-             next
-           end
-           Hiera.debug("Checking #{source} for #{scope_key} regex matching #{scope[scope_key]}")
+        answer = nil
+        datasourcefiles(scope) do |source|
+          scope_key = File.basename(source, ".regex")
+          scope_key = "::#{scope_key}" if ! scope[scope_key]
+          if ! scope[scope_key]
+            Hiera.debug("Could not find key #{scope_key} within scope, skipping check for source #{source}")
+            next
+          end
+          Hiera.debug("Checking #{source} for #{scope_key} regex matching #{scope[scope_key]}")
 
-           # TODO: Add support for more than just YAML format
-           data = YAML.load(@cache.read(source))
+          # TODO: Add support for more than just YAML format
+          data = YAML.load(@cache.read(source))
 
-           next if ! data
-           next if data.empty?
+          next if ! data
+          next if data.empty?
 
-           lineno = 0
-           data.each do |item|
-             lineno = lineno + 1
-             item.each_key do |regex_key|
-                if scope[scope_key] =~ /#{regex_key}/ and item[regex_key][key]
-                  Hiera.debug("#{scope_key} with value of '#{scope[scope_key]}' matched regex /#{regex_key}/ at #{source}:#{lineno}")
-                  new_answer = Backend.parse_answer(item[regex_key][key], scope)
-                  case resolution_type
-                  when :hash
-                    raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
-                    answer ||= {}
-                    answer = Backend.merge_answer(new_answer,answer)
-                  else
-                    answer = new_answer
-                    break
-                  end
+          lineno = 0
+          data.each do |item|
+            lineno = lineno + 1
+            item.each_key do |regex_key|
+              if scope[scope_key] =~ /#{regex_key}/ and item[regex_key][key]
+                Hiera.debug("#{scope_key} with value of '#{scope[scope_key]}' matched regex /#{regex_key}/ at #{source}:#{lineno}")
+                Hiera.debug("---- current key is: #{key} item: #{item}")
+                new_answer = Backend.parse_answer(item[regex_key][key], scope)
+                Hiera.debug("---- new_answer is: #{new_answer}")
+                if new_answer.is_a?(Array)
+                  answer << new_answer
+                elsif new_answer.is_a?(Hash)
+                  answer = Backend.merge_answer(new_answer,answer)
+                else
+                  answer = new_answer
+                  Hiera.debug("---- final answer is: #{answer}")
+                  return answer
                 end
-             end
-             break if answer
-           end
-         end
-         return answer
-       end
+                # case resolution_type
+                # when :hash
+                #   raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
+                #   Hiera.debug("@@@@ got inside hash case")
+                #   answer ||= {}
+                #   answer = Backend.merge_answer(new_answer,answer)
+                # else
+                #   answer = new_answer
+                #   return answer
+                #   # break
+                # end
+              end
+            end
+            # break if answer
+          end
+        end
+        Hiera.debug("---- final answer is: #{answer}")
+        return answer
+      end
 
-       def datasources(scope)
-         datadir = Backend.datadir(:regex, scope)
-         Config[:hierarchy].flatten.map do |source|
-           # We only support data sources that end in a key.  Keys found
-           # in the middle of a datasource's name are impossible to support since
-           # standard file names do not do well containing regular experssion patterns
-           if source =~ /\}$/
-             # We need to strip out the ending key's %{::} characters so that they do not
-             # get interpolated when we call parse_string
-             yield(Backend.parse_string(source.gsub(/%\{:*([^\{]*)\}$/, '\1'), scope))
-           end
-         end
-       end
+      def datasources(scope)
+        datadir = Backend.datadir(:regex, scope)
+        Config[:hierarchy].flatten.map do |source|
+          # We only support data sources that end in a key.  Keys found
+          # in the middle of a datasource's name are impossible to support since
+          # standard file names do not do well containing regular experssion patterns
+          if source =~ /\}$/
+            # We need to strip out the ending key's %{::} characters so that they do not
+            # get interpolated when we call parse_string
+            yield(Backend.parse_string(source.gsub(/%\{:*([^\{]*)\}$/, '\1'), scope))
+          end
+        end
+      end
 
-       def datasourcefiles(scope)
-         datadir = Backend.datadir(:regex, scope)
-         datasources(scope) do |source|
-           file = Backend.datafile(:regex, scope, source, :regex)
-           if file
-             yield(file)
-           end
-         end
-       end
-
+      def datasourcefiles(scope)
+        datadir = Backend.datadir(:regex, scope)
+        datasources(scope) do |source|
+          file = Backend.datafile(:regex, scope, source, :regex)
+          if file
+            yield(file)
+          end
+        end
+      end
     end
   end
-end   
+end
