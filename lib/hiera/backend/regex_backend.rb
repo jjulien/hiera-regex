0710135d306cf824c790a6354c532a631f1252aa
Calvin Leung Huang <cleung2010@gmail.com>
2015-01-05 10:33:42 -0500

    Clean up comments and debug output

--- lib/hiera/backend/regex_backend.rb
+++ lib/hiera/backend/regex_backend.rb
@@ -34,35 +34,19 @@ class Hiera
             item.each_key do |regex_key|
               if scope[scope_key] =~ /#{regex_key}/ and item[regex_key][key]
                 Hiera.debug("#{scope_key} with value of '#{scope[scope_key]}' matched regex /#{regex_key}/ at #{source}:#{lineno}")
-                Hiera.debug("---- current key is: #{key} item: #{item}")
                 new_answer = Backend.parse_answer(item[regex_key][key], scope)
-                Hiera.debug("---- new_answer is: #{new_answer}")
                 if new_answer.is_a?(Array)
                   answer << new_answer
                 elsif new_answer.is_a?(Hash)
                   answer = Backend.merge_answer(new_answer,answer)
                 else
                   answer = new_answer
-                  Hiera.debug("---- final answer is: #{answer}")
                   return answer
                 end
-                # case resolution_type
-                # when :hash
-                #   raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
-                #   Hiera.debug("@@@@ got inside hash case")
-                #   answer ||= {}
-                #   answer = Backend.merge_answer(new_answer,answer)
-                # else
-                #   answer = new_answer
-                #   return answer
-                #   # break
-                # end
               end
             end
-            # break if answer
           end
         end
-        Hiera.debug("---- final answer is: #{answer}")
         return answer
       end
 
